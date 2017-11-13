62f7877477a117a949b3286a6fea59ba9fa8ab72
Robert Halter <robert.halter@adcubum.com>
2017-11-13 16:49:33 +0100

    fdgfdg

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import java.util.List;
 
 public class Player {
 
@@ -12,17 +15,31 @@ public class Player {
         JsonElement potelement = myRequest.get("pot");
         int pot = potelement.getAsInt();
 
-        //JsonElement listElement = myRequest.get("players");
+        JsonElement listElement = myRequest.get("players");
 
-        //Array myPlayers = listElement.
-/**
-        if(pot > 7) {
-            return 500;
+        JsonArray myPlayers = listElement.getAsJsonArray();
+
+        JsonElement inedxelement = myRequest.get("in_action");
+
+
+        JsonArray myHand = myPlayers.get(inedxelement.getAsInt()).getAsJsonObject().get("hole_cards").getAsJsonArray();
+
+
+
+        if(myHand.get(0).getAsJsonObject().get("rank").getAsString().equals(myHand.get(1).getAsJsonObject().get("rank").getAsString())  ) {
+            return 1000;
         } else {
-            return 0;
+            return 500;
         }
-*/
-    return 354;
+
+/**
+ if(pot > 7) {
+ return 500;
+ } else {
+ return 0;
+ }
+ */
+
 
     }
 
