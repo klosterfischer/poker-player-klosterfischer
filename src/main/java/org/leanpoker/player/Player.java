cee1615a9c0169a71ff51015a59d0cfbff044b0b
Robert Halter <robert.halter@adcubum.com>
2017-11-13 15:38:25 +0100

    fdgf

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,29 +1,28 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
-
-import java.util.Map;
+import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        JsonObject myRequest = request.getAsJasonObject();
+        JsonObject myRequest = request.getAsJsonObject();
         JsonElement potelement = myRequest.get("pot");
         int pot = potelement.getAsInt();
 
         //JsonElement listElement = myRequest.get("players");
 
         //Array myPlayers = listElement.
-
+/**
         if(pot > 7) {
             return 500;
         } else {
             return 0;
         }
-
-
+*//
+    return 70;
 
     }
 
