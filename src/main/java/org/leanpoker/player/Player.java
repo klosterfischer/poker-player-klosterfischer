f643db15871104c8b7874976ba518783614c7cb1
MSchweisfurt <michael.schweisfurthadcubum.com>
2017-11-13 12:04:15 +0100

    100

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        System.out.println( reqest.toString());
+        System.out.println( request.toString());
         return 100;
 
     }
