08ef9651a935bb8146cf3deaeea0162fbecc28e7
MSchweisfurt <michael.schweisfurthadcubum.com>
2017-11-13 11:40:18 +0100

    ja

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return current_buy_in - players[in_action][bet];
+        return current_buy_in - players[in_action][bet] + minimum_raise;
     }
 
     public static void showdown(JsonElement game) {
